10e7d6b7ba9af1093cd00ee5ab6c9997f830447a
LukaszKot <lukaszkot03@gmail.com>
2021-04-22 00:08:59 +0200

    Removed Main function and Program class.

--- Program.cs
+++ Program.cs
@@ -1,19 +1,10 @@
 ﻿using System;
 using SuperMac.Utilities;
 
-namespace SuperMac
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Witaj w SuperMac! Najlepszej terminalowej restauracji w .NET! U nas zawsze zjesz dobrze, smacznie i szybko!");
+Console.WriteLine("Witaj w SuperMac! Najlepszej terminalowej restauracji w .NET! U nas zawsze zjesz dobrze, smacznie i szybko!");
 
-            OptionsManager.Question("Co chcesz zrobić?")
-                .AddOption("Zamówić na miejscu.", ()=> Console.WriteLine("test1"))
-                .AddOption("Zamówić na wynos", ()=> Console.WriteLine("test2"))
-                .AddOption("Wyjść", ()=> Environment.Exit(0))
-                .Ask();
-        }
-    }
-}
+OptionsManager.Question("Co chcesz zrobić?")
+    .AddOption("Zamówić na miejscu.", ()=> Console.WriteLine("test1"))
+    .AddOption("Zamówić na wynos.", ()=> Console.WriteLine("test2"))
+    .AddOption("Wyjść.", ()=> Environment.Exit(0))
+    .Ask();
